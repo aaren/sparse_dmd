7c485bfbf035887da6a2ac9e56b2a612a7c7c1ff
Aaron O'Leary <aaron.oleary@gmail.com>
2014-04-30 17:06:09 +0100

    package as module

---
+++ setup.py
@@ -0,0 +1,17 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+        'description': 'Sparse Dynamic Mode Decomposition',
+        'author': "Aaron O'Leary",
+        'url': 'http://github.com/aaren/sparse_dmd',
+        'author_email': 'eeaol@leeds.ac.uk',
+        'version': '0.1',
+        'packages': ['sparse_dmd'],
+        'scripts': [],
+        'name': 'sparse_dmd',
+        }
+
+setup(**config)
---
+++ sparse_dmd/__init__.py
@@ -0,0 +1,3 @@
+from sparse_dmd import SparseDMD, SparsePlots
+from sparse_dmd import run_dmdsp
+from sparse_dmd import to_data, to_snaps
